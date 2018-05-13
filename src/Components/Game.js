edf125be0cdca03e8d10a2c531be269293426b7e
prameshbajra <pe.messh@gmail.com>
2018-05-13 23:18:50 +0530

    This doesnt seem to work, fifure out a way to make it work now :D

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -6,25 +6,51 @@ class Game extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            sequence: [],
+            sequence: ['RED'],
             steps: 0,
+            message: null,
             colors: ['RED', 'GREEN', 'BLUE', 'YELLOW', 'BLACK']
         }
     }
 
-    sequenceAdder = (color) => {
-        this.setState(() => ({ sequence: [...this.state.sequence, color] }));
+    nextSqeuence = () => {
+        this.setState(() => ({
+            sequence: [...this.state.sequence,
+            this.state.colors[Math.floor(Math.random() * this.state.colors.length - 1)]]
+        }));
+        console.log('New sequence', this.state.sequence);
     }
+
+    sequenceAdder = (color) => {
+        if (color === this.state.sequence[this.state.steps]) {
+            if (this.state.sequence.length - 1 === this.state.steps) {
+                this.setState(() => ({ steps: 0 }));
+                this.nextSqeuence();
+                console.log('one', this.state.sequence);
+            } else {
+                this.setState(() => ({
+                    steps: +this.state.steps + 1
+                }));
+                console.log('two', this.state.sequence);
+            }
+        } else {
+            this.setState(() => ({
+                message: "You know you just lost right??",
+                steps: 0, sequence: []
+            }))
+            console.log('three', this.state.sequence);
+        }
+    }
+
     render() {
-        console.log(this.state.sequence);
         return (
             <div>
+                {this.state.message}
                 {
                     this.state.colors.map((color) => (
                         <Colors color={color} key={color} sequenceAdder={this.sequenceAdder} />
                     ))
                 }
-
                 <br /><br /><br />
             </div>
         );
